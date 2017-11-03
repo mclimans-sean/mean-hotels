3febd6c598c14b9288cc1bc14e05b4e84346f5ec
SEAN MCLIMANS <seanmclimans@gmail.com>
2017-11-03 17:03:30 -0600

    add POST route for one hotel

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,8 +7,13 @@ router
   .route('/hotels')
   .get(ctrlHotels.hotelsGetAll);
 
-  router
-    .route('/hotels/:hotelId')
-    .get(ctrlHotels.hotelsGetOne);
+router
+  .route('/hotels/:hotelId')
+  .get(ctrlHotels.hotelsGetOne);
+
+router
+  .route('/hotels/new')
+  .post(ctrlHotels.hotelsAddOne);
+
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const routes = require('./api/routes');
 
 app.set('port', 3000);
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/api', routes);
 
