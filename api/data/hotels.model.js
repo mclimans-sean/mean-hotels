01f923b54fbdfb2e84c91f7a3a0a0031b7e5954c
SEAN MCLIMANS <seanmclimans@gmail.com>
2017-11-15 15:53:50 -0700

    Add Schemas for review and rooms

--- api/data/hotels.model.js
+++ api/data/hotels.model.js
@@ -1,5 +1,34 @@
 const mongoose = require('mongoose');
 
+const reviewSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+    required: true
+  },
+  review: {
+    type: String,
+    required: true
+  },
+  createdOn: {
+    type: Date,
+    "default": Date.now
+  }
+});
+
+const roomSchema = new mongoose.Schema({
+  type: String,
+  number: Number,
+  description: String,
+  photos: [String],
+  price: Number
+});
+
 const hotelSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -14,7 +43,16 @@ const hotelSchema = new mongoose.Schema({
   services: [String],
   description: String,
   photos: [String],
-  currency: String
+  currency: String,
+  reviews: [reviewSchema],
+  rooms: [roomSchema],
+  location: {
+    address: String,
+    coordinates: {
+      type: [Number],
+      index: '2dsphere'
+    }
+  }
 });
 
 mongoose.model('Hotel', hotelSchema)
