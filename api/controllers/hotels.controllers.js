e52aaeb2f04d42501c900eb9dfc7bd38166b5228
SEAN MCLIMANS <seanmclimans@gmail.com>
2017-11-17 15:33:10 -0700

    error trapping for GET routes for hotels

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -33,6 +33,7 @@ module.exports.hotelsGetAll = function(req, res) {
 
   var offset = 0;
   var count = 5;
+  var maxCount = 10;
 
   if (req.query && req.query.lat && req.query.lng) {
     runGeoQuery(req, res);
@@ -47,10 +48,38 @@ module.exports.hotelsGetAll = function(req, res) {
     count = parseInt(req.query.count, 10);
   }
 
+  if (isNaN(offset) || isNaN(count)) {
+    res
+      .status(400)
+      .json({
+        "message": "If supplied in querystring, count and offset should be numbers"
+      });
+    return;
+  }
+
+  if (count > maxCount) {
+    res
+      .status(400)
+      .json({
+        "message": "Count limit of " + maxCount + " exceeded"
+      });
+      return;
+  }
+
   Hotel.find()
     .skip(offset)
     .limit(count)
     .exec(function(err, hotels) {
+      if (err) {
+        console.log("Error finding hotels");
+        res
+          .status(500)
+          .json(err);
+      } else {
+        console.log("Found hotels", hotels.length);
+        res
+          .json(hotels);
+      }
       console.log("Found hotels", hotels.length);
       res.json(hotels)
     })
@@ -63,13 +92,27 @@ module.exports.hotelsGetOne = function(req, res) {
 
   console.log('GET hotelId', hotelId);
 
-  Hotel.findById(hotelId)
+  Hotel
+    .findById(hotelId)
     .exec(function(err, doc) {
+      var response = {
+        status: 200,
+        message: doc
+      };
+      if (err) {
+        console.log("Error finding hotel");
+        response.status = 500;
+        response.message = err;
+      } else if (!doc) {
+        response.status = 404;
+        response.message = {
+            "message": "Hotel ID not found"
+          };
+      }
       res
-        .status(200)
-        .json(doc);
+        .status(response.status)
+        .json(response.message);
   });
-
 };
 
 module.exports.hotelsAddOne = function(req, res) {
