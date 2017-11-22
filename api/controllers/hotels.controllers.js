e59a26bda5e4aea0a88a0660467bb1253d071a0e
SEAN MCLIMANS <seanmclimans@gmail.com>
2017-11-22 11:08:40 -0700

    add GET/POST routes for reviews

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -115,31 +115,44 @@ module.exports.hotelsGetOne = function(req, res) {
   });
 };
 
-module.exports.hotelsAddOne = function(req, res) {
-  console.log("POST new hotel");
-  var db = dbconn.get();
-  var collection = db.collection('hotels');
-  var newHotel;
-
-  if (req.body && req.body.name && req.body.stars) {
-    newHotel = req.body;
-    newHotel.stars = parseInt(req.body.stars, 10);
-    collection.insertOne(newHotel, function(err, response) {
-      console.log("Hotel added", response);
-      console.log("Hotel added", response.ops);
-      res
-        .status(201)
-        .json(response.ops);
-    });
-    // console.log(newHotel);
-    // res
-    //   .status(200)
-    //   .json(newHotel);
+var _splitArray = function(input) {
+  var output;
+  if (input && input.length > 0) {
+    output = input.split(";")
   } else {
-    console.log("Data missing from body");
-    res
-      .status(400)
-      .json({ message : "Required data missing from body" });
+    output = [];
   }
+  return output;
+};
 
+module.exports.hotelsAddOne = function(req, res) {
+
+  Hotel
+    .create({
+      name: req.body.name,
+      description: req.body.description,
+      stars: parseInt(req.body.stars, 10),
+      services: _splitArray(req.body.services),
+      photos: _splitArray(req.body.photos),
+      currency: req.body.currency,
+      location: {
+        address: req.body.address,
+        coordinates: [
+          parseFloat(req.body.lng),
+          parseFloat(req.body.lat)
+        ]
+      }
+    }, function(err, hotel) {
+      if (err) {
+        console.log("Error creating hotel");
+        res
+          .status(400)
+          .json(err)
+      } else {
+        console.log("Hotel created", hotel);
+        res
+          .status(201)
+          .json(hotel)
+      }
+    });
 };
--- api/controllers/reviews.controllers.js
+++ api/controllers/reviews.controllers.js
@@ -33,3 +33,58 @@ module.exports.reviewsGetOne = function(req, res) {
         .json(review);
   });
 };
+
+var _addReview = function(req, res, hotel) {
+
+  hotel.reviews.push({
+    name: req.body.name,
+    rating: parseInt(req.body.rating, 10),
+    review: req.body.review
+  });
+
+  hotel.save(function(err, hotelUpdated) {
+    if (err) {
+      res
+        .status(500)
+        .json(err);
+    } else {
+      res
+        .status(201)
+        .json(hotelUpdated.reviews[hotelUpdated.reviews.length - 1])
+    }
+  })
+}
+
+module.exports.reviewsAddOne = function(req, res) {
+
+  var hotelId = req.params.hotelId;
+  console.log('GET hotelId', hotelId);
+
+  Hotel
+    .findById(hotelId)
+    .select('reviews')
+    .exec(function(err, doc) {
+      var response = {
+        status: 200,
+        message: []
+      };
+      if (err) {
+        console.log("Error finding hotel");
+        response.status = 500;
+        response.message = err;
+      } else if (!doc) {
+        console.log("Hotel id not found in database", id);
+        response.statuses = 404;
+        response.message = {
+          "message" : "Hotel id not found " + id
+        };
+      }
+      if (doc) {
+        _addReview(req, res, doc);
+      } else {
+        res
+          .status(response.status)
+          .json(response.message)
+      }
+  });
+};
